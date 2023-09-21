d71ce652897537c75ddd2708f7023ec34fab4da2
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-09-21 21:43:41 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+let todos = ["Usman", "Ali", "Saeed"];
+todos.splice(1, 0, "Talha", "Hamza");
+console.log(todos);
+//Create table
+var table1 = 1;
+while (table1 <= 10) {
+    let result = table1 * 5;
+    console.log("5 * " + table1 + " = " + result);
+    table1 = table1 + 1;
+}
+var table1 = 1;
+while (table1 <= 10) {
+    let result = table1 * 9;
+    console.log("9 * " + table1 + " = " + result);
+    table1 = table1 + 1;
+}
---
+++ index.ts
@@ -0,0 +1,21 @@
+let todos: string[] = ["Usman", "Ali", "Saeed"];
+todos.splice(1, 0, "Talha", "Hamza");
+console.log(todos);
+
+//Create table
+
+var table1: number = 1;
+while (table1 <= 10) {
+    let result: number = table1 * 5
+    console.log("5 * " + table1 + " = " + result);
+    table1 = table1 + 1;
+
+}
+
+var table1: number = 1;
+while (table1 <= 10) {
+    let result: number = table1 * 9
+    console.log("9 * " + table1 + " = " + result);
+    table1 = table1 + 1;
+
+}
